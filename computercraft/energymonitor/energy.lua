5c7595b479ff364f2c17329194bce85e4a425087
IndusRobot <whiz_kid10@yahoo.com>
2024-07-31 09:40:01 -0600

    peripheral changes

--- computercraft/energymonitor/energy.lua
+++ computercraft/energymonitor/energy.lua
@@ -4,7 +4,8 @@ local EnergyMonitor = require( "EnergyMonitor" )
 print( "- Building energy monitors" )
 
 local monitorParams = {
-    peripheral_name = "myMonitor1"
+    monitorId = "myMonitor1",
+    peripheral_name = "monitor_0"
 }
 
 -- This function is called directly before the graph is updated. If there are any delays or async handling it won't look right! 
